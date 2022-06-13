cfd7f3990669a494d0bb417525701a2806de9359
Jeff <j.edjekoomhene@gmail.com>
2022-06-13 22:13:23 +0200

    initialize db model

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    complete = db.Column(db.Boolean)
+    
+db.create_all()
 
 @app.route('/')
 def index():
